66466f474012b2659df7d135f4c8c53290b0c455
Mitch <Mitch@Tablet>
2015-02-05 17:21:40 -0700

    Initial Commit

---
+++ TheAmericanLife/src/me/MitchT/AmericanLife/Main.java
@@ -0,0 +1,9 @@
+package me.MitchT.AmericanLife;
+
+public class Main {
+
+	public static void main(String[] args) {
+		new MainMenu().setVisible(true);
+	}
+	
+}
---
+++ TheAmericanLife/src/me/MitchT/AmericanLife/MainMenu.java
@@ -0,0 +1,62 @@
+package me.MitchT.AmericanLife;
+
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class MainMenu extends JFrame implements ActionListener {
+
+	JButton[] menuButtons = new JButton[3];
+
+	public MainMenu() {
+		JPanel mainPanel = new JPanel();
+		mainPanel.setLayout(new BorderLayout());
+
+		this.setContentPane(mainPanel);
+
+		this.setPreferredSize(new Dimension(800,600));
+		this.setResizable(false);
+		
+		JPanel buttonPanel = new JPanel();
+		buttonPanel.setLayout(new GridLayout(3, 1));
+		getContentPane().add(buttonPanel, BorderLayout.CENTER);
+		
+		JButton startButton = new JButton("Start Game");
+		JButton optionsButton = new JButton("Options");
+		JButton quitButton = new JButton("Quit");
+
+		menuButtons[0] = startButton;
+		menuButtons[1] = optionsButton;
+		menuButtons[2] = quitButton;
+
+		for (JButton button : menuButtons) {
+			button.addActionListener(this);
+			buttonPanel.add(button);
+		}
+		
+		this.pack();
+		this.setLocationRelativeTo(null);
+		this.setVisible(false);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+
+		if (arg0.getSource().equals(menuButtons[0])) { // Play
+
+		} else if (arg0.getSource().equals(menuButtons[1])) // Options
+		{
+
+		} else if (arg0.getSource().equals(menuButtons[2])) // Quit
+		{
+			System.exit(0);
+		}
+
+	}
+}
